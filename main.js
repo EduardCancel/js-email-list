7cac78804e37891a82f90fb72d0e9730bd28f832
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-27 15:47:30 +0100

    add cicle and api

---
+++ main.js
@@ -0,0 +1,22 @@
+const emailListEl = document.getElementById('email-list')
+
+const listEmailGenerate = []
+
+// Add cicle for the email
+
+for (let i = 0; i < 10; i++){
+
+    fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then(response => response.json())
+        .then(data => {
+            listEmailGenerate.push(data.response)
+
+            const li = document.createElement('li')
+            li.textContent = data.response;
+            emailListEl.appendChild(li)
+        }
+        )
+        .catch((error) => console.error(error));
+}
+
+console.log(listEmailGenerate);
