85b634cc85530c6efc01d4635f672d77e70de9aa
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-27 16:29:23 +0100

    add bonus button

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const emailListEl = document.getElementById('email-list')
+const buttoneEl = document.querySelector('.btn')
 
 const listEmailGenerate = []
 
 // Add cicle for the email
 
-for (let i = 0; i < 10; i++){
+/* for (let i = 0; i < 10; i++){
 
     fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
         .then(response => response.json())
@@ -17,7 +18,28 @@ for (let i = 0; i < 10; i++){
         }
         )
         .catch((error) => console.error(error));
-}
+} */
 
 console.log(listEmailGenerate);
 
+buttoneEl.addEventListener("click",() =>{
+
+    emailListEl.innerHTML = ""
+    listEmailGenerate.length = 0;
+    
+    for (let i = 0; i < 10; i++){
+
+        fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then(response => response.json())
+            .then(data => {
+                listEmailGenerate.push(data.response)
+    
+                const li = document.createElement('li')
+                li.textContent = data.response;
+                emailListEl.appendChild(li)
+            }
+            )
+            .catch((error) => console.error(error));
+    }
+    
+})
